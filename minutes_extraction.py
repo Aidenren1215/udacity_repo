fb54216673fa2929ff9c9256360377726af01a35
Aidenren1215 <aidenren1215@gmail.com>
2026-01-29 11:29:31 +0800

    re changes

--- minutes_extraction.py
+++ minutes_extraction.py
@@ -18,24 +18,26 @@ END_MARKER = "GROUP BALANCE SHEET STRATEGY"
 
 DATE_RE = re.compile(
     r"\b(\d{1,2})\s+"
-    r"(January|February|March|April|May|June|July|August|September|October|November|December)"
+    r"(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|"
+    r"Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:ember)?|"
+    r"Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)"
     r"\s+(\d{4})\b",
     re.IGNORECASE,
 )
 
 _MONTHS = {
-    "january": 1,
-    "february": 2,
-    "march": 3,
-    "april": 4,
+    "jan": 1, "january": 1,
+    "feb": 2, "february": 2,
+    "mar": 3, "march": 3,
+    "apr": 4, "april": 4,
     "may": 5,
-    "june": 6,
-    "july": 7,
-    "august": 8,
-    "september": 9,
-    "october": 10,
-    "november": 11,
-    "december": 12,
+    "jun": 6, "june": 6,
+    "jul": 7, "july": 7,
+    "aug": 8, "august": 8,
+    "sep": 9, "sept": 9, "september": 9,
+    "oct": 10, "october": 10,
+    "nov": 11, "november": 11,
+    "dec": 12, "december": 12,
 }
 
 
